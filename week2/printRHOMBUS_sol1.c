13934573f663e669102a9164bd9388378424f126
ppondeu <106013516+ppondeu@users.noreply.github.com>
2022-08-09 11:22:09 +0700

    Update printRHOMBUS_sol1.c

--- week2/printRHOMBUS_sol1.c
+++ week2/printRHOMBUS_sol1.c
@@ -1,1 +1,31 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    int begin=n;
+    int end=n;
+
+    for(int i=1;i<=2*n-1;i++){
+        for(int j=1;j<=2*n-1;j++){
+            if(j>=begin&&j<=end){
+                printf("*");
+            }else{
+                printf(" ");
+            }
+            
+        }
+        if(i<n){
+                begin--;
+                end++;
+            }else{
+                begin++;
+                end--;
+            }
+        printf("\n");
+    }
+    char ch = getchar();
+    return 0;
+}
 
