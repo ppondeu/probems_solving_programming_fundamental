434fce71fe6f7e1a296b836c285761983ec1f746
ppondeu <106013516+ppondeu@users.noreply.github.com>
2022-09-07 18:19:59 +0700

    Update writeTextfile_fputs.c

--- week5/writeTextfile_fputs.c
+++ week5/writeTextfile_fputs.c
@@ -2,6 +2,23 @@
 #include<string.h>
 
 void putsfile(){
+    char data[1001]="";
+    FILE *f = fopen("file_fputs.txt", "a");
+    while(1){
+        char input[1001];
+        scanf("%[^\n]*c ", input);
+        fflush(stdin);
+        strcat(data, input);
+        strcat(data, "\n");
+        if(strcmp(input, ".")==0){
+            break;
+        }
+    }
+    fputs(data,f);
+    fclose(f);
+}
+
+/*void putsfile2(){
     FILE *f = fopen("file_fputs.txt", "a");
     while(1){
         char input[1001];
@@ -14,7 +31,7 @@ void putsfile(){
         }
     }
     fclose(f);
-}
+}*/
 
 int main(){
     printf("Input data string:");
